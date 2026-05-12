8844d05346a0d88bfed4fd9f03915b94818c0195
greenwave1987 <48582745+greenwave1987@users.noreply.github.com>
2026-05-12 15:51:21 +0800

    Update renew.py

--- freecloud/renew.py
+++ freecloud/renew.py
@@ -44,7 +44,7 @@ def run_task():
             "--disable-gpu",
             f"--proxy-server={proxy_url}",
             "--disable-blink-features=AutomationControlled",
-            "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
+            #"--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
         ]
     )
     
